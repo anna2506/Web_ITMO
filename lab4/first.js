41d0e5df94731cb47c6d65af150ccb5bdeaf0d2f
Anna Doshchenko <annadoshenko3@gmail.com>
2020-04-18 15:47:52 +0300

    Started lab4

---
+++ lab4/first.js
@@ -0,0 +1,96 @@
+/*const second = require('second.js');
+module.exports = function render(){
+    return second.render();
+};*/
+
+function first(){
+    document.body.setAttribute("style", "background: #9477e4; margin: 0; padding: 0");
+    var photo = document.createElement('div');
+    var img = document.createElement('img');
+    photo.style.margin = "10px";
+    photo.style.display = "flex";
+    photo.style.justifyContent = "center";
+    img.src = "images/me.jpg";
+    img.style.borderRadius = "45%";
+    photo.appendChild(img);
+    document.body.appendChild(photo);
+
+    var list = document.createElement("li");
+    var ul = document.createElement('ul');
+    list.innerHTML = "Дощенко Анна";
+    ul.appendChild(list);
+
+
+    const name = document.createElement('h1');
+    var info = document.createElement("div");
+    info.style.display = "flex";
+    info.style.flex = "1";
+    info.style.justifyContent = "center";
+    info.style.flexWrap = "wrap";
+    name.innerHTML = "Дощенко Анна";
+    name.style.color = "#bf0d3d";
+    name.style.display = "flex";
+    name.style.width = "100%";
+    name.style.justifyContent = "center";
+    name.style.margin = "3px";
+    name.style.fontFamily = "Brush Script Std, cursive";
+    info.appendChild(name);
+    let restInfo = document.createElement("p");
+    restInfo.className = "personal.info";
+    restInfo.style.display = "flex";
+    restInfo.style.justifyContent = "center";
+    restInfo.style.flex = "1";
+    restInfo.style.textAlign = "center";
+    restInfo.style.padding = "5px";
+    restInfo.innerHTML = "Студентка группы М3307. Член сборной по алтимат-фрисби. </br> В свободное время люблю проводить время с друзьями и заниматься спортом.";
+    info.appendChild(restInfo);
+    restInfo.style.color = "#070311";
+    restInfo.style.fontSize = "16pt";
+    restInfo.style.margin = "0";
+    restInfo.style.fontFamily = "Brush Script Std, cursive";
+    document.body.appendChild(info);
+
+
+    var button = document.createElement("div");
+    var pockemonButton = document.createElement("button");
+    pockemonButton.innerHTML = "Get pockemon";
+    pockemonButton.style.background = "none";
+    pockemonButton.style.border = "3px solid #070311";
+    pockemonButton.style.fontWeight = "bold";
+    pockemonButton.style.cursor = "pointer";
+    pockemonButton.style.borderRadius = "15px";
+    pockemonButton.style.height = "50px";
+    pockemonButton.style.width = "150px";
+    pockemonButton.style.color = "#070311";
+    pockemonButton.style.fontSize = "12pt";
+    hover = pockemonButton.addEventListener("mouseenter", function () {
+        pockemonButton.style.transitionDuration = "0.3s";
+        pockemonButton.style.backgroundColor = "#bf0d3d";
+        pockemonButton.style.border = "3px solid #bf0d3d";
+        pockemonButton.style.color = "white";
+    });
+    leave = pockemonButton.addEventListener("mouseleave", function () {
+        pockemonButton.style.transitionDuration = "0.3s";
+        pockemonButton.style.background = "none";
+        pockemonButton.style.border = "3px solid #070311";
+        pockemonButton.style.color = "#070311";
+    });
+    button.style.margin = "30px";
+    button.style.display = "flex";
+    button.style.justifyContent = "center";
+    button.appendChild(pockemonButton);
+    document.body.appendChild(button);
+
+    click = pockemonButton.addEventListener("click", async function () {
+    });
+
+}
+
+module.exports = () => {
+    return first();
+}
+
+
+
+
+
---
+++ lab4/second.js
@@ -0,0 +1,119 @@
+
+function render() {
+    document.body.setAttribute("style", "background: #02ded3; margin: 0; padding: 0");
+    var name = document.createElement('span');
+    name.style.display = "flex";
+    name.style.justifyContent = "center";
+    name.style.padding = "20px";
+    name.style.fontWeight = "bold";
+    name.style.fontSize = "30pt";
+    name.style.color = "#6a2bfc";
+    name.style.fontFamily = "Brush Script Std, cursive";
+    var abilities = document.createElement('div');
+    const header = document.createElement('h1');
+    header.style.color = "#0c013c";
+    header.style.display = "flex";
+    header.style.justifyContent = "center";
+    header.style.margin = "0";
+    header.style.fontFamily = "Brush Script Std, cursive";
+    header.innerHTML = "Abilities";
+
+    async function getPockemon() {
+        let id = Math.floor(Math.random() * (808));
+        let url = 'https://pokeapi.co/api/v2/pokemon/' + id.toString() + '/';
+        const response = await fetch(url, {method: 'GET'});
+        const json = await response.json();
+        name.innerHTML = json.name;
+        const array = [];
+        for (let i = 0; i < json.abilities.length; i++) {
+            console.log(i);
+            array.push(json.abilities[i]);
+            abilityName = json.abilities[i].ability.name;
+            abilityURL = json.abilities[i].ability.url;
+            var div = document.createElement('div');
+            div.style.display = "flex";
+            div.style.justifyContent = "center";
+            div.style.flex = "1";
+            div.style.alignItems = "center";
+            div.style.flexWrap = "wrap";
+            div.style.width = "300px";
+            div.style.border = "2px solid #6a2bfc";
+            div.style.borderRadius = "15px";
+            div.style.margin = "10px auto";
+            div.style.padding = "5px";
+            var ability = document.createElement('span');
+            ability.style.display = "flex";
+            ability.style.justifyContent = "center";
+            ability.style.color = "#6a2bfc";
+            ability.style.margin = '5px';
+            ability.style.fontWeight = 'bold';
+            ability.style.fontSize = '14pt';
+            ability.style.fontFamily = "Brush Script Std, cursive";
+            var description = document.createElement('p');
+            description.style.margin = "0";
+            description.style.textAlign = "center";
+            description.style.color = "#0c013c";
+            description.style.fontFamily = "Brush Script Std, cursive";
+            ability.innerHTML = abilityName;
+            div.appendChild(ability);
+            document.body.appendChild(div);
+
+            async function apiCall(url) {
+                const response = await fetch(url, {method: 'GET'});
+                const json = await response.json();
+                var desr = json.effect_entries[0].effect;
+                console.log(desr);
+                description.innerHTML = desr;
+                div.appendChild(description);
+            }
+
+            await apiCall(abilityURL);
+            abilities.appendChild(div);
+        }
+    }
+
+    getPockemon();
+    document.body.appendChild(name);
+    abilities.appendChild(header);
+    document.body.appendChild(abilities);
+
+    var button = document.createElement("div");
+    var personalInfoButton = document.createElement("button");
+    personalInfoButton.innerHTML = "Get personal Info";
+    personalInfoButton.style.background = "none";
+    personalInfoButton.style.border = "3px solid #0c013c";
+    personalInfoButton.style.fontWeight = "bold";
+    personalInfoButton.style.cursor = "pointer";
+    personalInfoButton.style.borderRadius = "15px";
+    personalInfoButton.style.height = "50px";
+    personalInfoButton.style.width = "150px";
+    personalInfoButton.style.color = "#0c013c";
+    personalInfoButton.style.fontSize = "12pt";
+    hover = personalInfoButton.addEventListener("mouseenter", function () {
+        personalInfoButton.style.transitionDuration = "0.3s";
+        personalInfoButton.style.backgroundColor = "#6a2bfc";
+        personalInfoButton.style.border = "3px solid #6a2bfc";
+        personalInfoButton.style.color = "white";
+    });
+    leave = personalInfoButton.addEventListener("mouseleave", function () {
+        personalInfoButton.style.transitionDuration = "0.3s";
+        personalInfoButton.style.background = "none";
+        personalInfoButton.style.border = "3px solid #0c013c";
+        personalInfoButton.style.color = "#0c013c";
+    });
+    button.style.margin = "30px";
+    button.style.display = "flex";
+    button.style.justifyContent = "center";
+    button.appendChild(personalInfoButton);
+    document.body.appendChild(button);
+
+    click = personalInfoButton.addEventListener("click", async function () {
+
+    });
+
+}
+
+render();
+module.exports = () => {
+    return render();
+}
